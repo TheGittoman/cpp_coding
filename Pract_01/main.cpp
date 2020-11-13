66ff052e20e115c7b192f23ff2d8862dba3bc299
TheGittoman <anonyme.and.free@protonmail.com>
2020-11-13 08:02:44 +0200

    VSCODE lauch and task.json added

---
+++ Pract_01/main.cpp
@@ -0,0 +1,10 @@
+#include "src/header.hpp"
+#include <iostream>
+
+int main()
+{
+
+    test_Func();
+    system("pause");
+    return 0;
+}
---
+++ Pract_01/src/header.cpp
@@ -0,0 +1,7 @@
+#include "header.hpp"
+#include <iostream>
+
+void test_Func()
+{
+    std::cout << "Hello World!" << std::endl;
+}
