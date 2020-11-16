edabc4b4407fe747280713bf3359c3e1019c2d8a
TheGittoman <anonyme.and.free@protonmail.com>
2020-11-16 08:17:14 +0200

    update

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -1,7 +1,22 @@
-#include "src/header.hpp"
-#include <iostream>
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#	include <wx/wx.h>
+#endif 
+#include "src/base.h"
 
-int main()
+IMPLEMENT_APP(MainApp) // A macro that tells wxWidgets to create an instance of our application 
+
+bool MainApp::OnInit() 
 {
-    return 0;
+	// Create an instance of our frame, or window 
+	MainFrame *MainWin = new MainFrame(_("Hello World!"), wxDefaultPosition, wxSize(300, 200));
+	MainWin->Show(true); // show the window 
+	SetTopWindow(MainWin); // and finally, set it as the main window 
+	return true;
+} 
+
+MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size) 
+: wxFrame((wxFrame *) NULL, -1, title, pos, size) 
+{
+	// normally we would initialize objects such as buttons and textboxes here 
 }
--- Pract_02/src/header.cpp
+++ Pract_02/src/header.cpp
@@ -1,18 +1,2 @@
 #include "header.hpp"
 #include <iostream>
-
-void cls()
-{
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-}
