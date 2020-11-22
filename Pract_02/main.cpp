310f99be7353b0b93e4a1f9c67c73fd796e37d53
TheGittoman <anonyme.and.free@protonmail.com>
2020-11-22 21:22:09 +0200

    jotain

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -1,12 +1,10 @@
-#include "src/base.h"
+#include "src/base.hpp"
 #include "src/header.hpp"
 #include <iostream>
 
 
 int main()
 {
-	int i = 20;
-	std::cout << i << std::endl;
 	system("pause");
 	return 0;
 }
--- Pract_02/src/header.cpp
+++ Pract_02/src/header.cpp
@@ -1,11 +1,2 @@
 #include "header.hpp"
 #include <iostream>
-
-void countTo(const int &limit)
-{
-    for (int i = 0; i <= limit; i++)
-    {
-        std::cout << i << std::endl;
-    }
-}
-
