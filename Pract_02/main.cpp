ca338b9207cfb26e8afce3d7bfd3f287345e52fd
TheGittoman <anonyme.and.free@protonmail.com>
2020-11-28 12:44:55 +0200

    Leetcode Dublicate zeros 12ms solution

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -7,9 +7,26 @@
 #include <string>
 #include <vector>
 
-
 int main()
 {
+	std::vector<int> arr{0,0,0};
+	std::vector<int> tempArr;
+	int arraySize = arr.size();
+	for(int i = 0; i < arraySize;++i)
+	{
+		tempArr.push_back(arr[i]);
+		if(arr[i] == 0)
+		{
+			if(i != arraySize - 1)
+			{
+				--arraySize;
+				tempArr.push_back(0);
+			}
+		}
+	}
+	arr = tempArr;
+	for(auto &element : arr)
+		std::cout << element << ",";
 	system("pause");
 	return 0;
 }
