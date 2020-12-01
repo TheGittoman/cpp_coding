4368f46a46bef8e6c783b9d1df59e3fa051c006a
TheGittoman <anonyme.and.free@protonmail.com>
2020-12-01 20:28:16 +0200

    commented the linked list code

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -16,12 +16,12 @@ struct Node
 };
 
 
-void insert(Node **pointerToHead, int num)
+void insert(Node **pointerToHead, int num) // double pointer because of passing pointer by reference
 {
-	Node *temp = new Node();
-	temp->data = num;
-	temp->next = *pointerToHead;
-	*pointerToHead = temp;
+	Node *temp = new Node();		// greates new Node pointer and points it at newly greated Node object
+	temp->data = num; 				// allocates new Node with data
+	temp->next = *pointerToHead;	// allocatest the new node's next to point the old Node.
+	*pointerToHead = temp;			// makes the previous Node to point to the new making temp the first.
 }
 
 void print(Node *head)
@@ -29,7 +29,7 @@ void print(Node *head)
 	while(head != NULL)
 	{
 		std::cout << head->data;
-		head = head->next;
+		head = head->next;			// modifying head->next to point the next Node of the current Node
 	}
 }
 
@@ -42,7 +42,7 @@ int main()
 	{
 		int num;
 		std::cin >> num;
-		insert(&head, num);
+		insert(&head, num);			// passing head by reference to the insert function
 		print(head);
 	}
 	system("pause");
