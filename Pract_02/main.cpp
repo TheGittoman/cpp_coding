35c160b729900a8033f1378d14ec1cad41270986
TheGittoman <anonyme.and.free@protonmail.com>
2020-12-12 23:55:57 +0200

    changes

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -8,19 +8,15 @@
 #include <vector>
 #include <climits>
 
+std::vector<int> replaceElements(std::vector<int> &arr)
+{
+
+}
 
 int main()
 {
 	std::vector<int> nums{0, 2, 3, 4, 5, 2, 1, 0};
-	std::vector<int> nums2{7, 2, 5, 3};
-	std::vector<int> nums3{-1, 2, -5, 5, 3, 2};
-	std::vector<int> nums4{1, 2, 3, 4, 5};
-	std::vector<int> nums5{5, 4, 3, 2, 1};
-	std::cout << validMountainArray(nums) << std::endl;
-	std::cout << validMountainArray(nums2) << std::endl;
-	std::cout << validMountainArray(nums3) << std::endl;
-	std::cout << validMountainArray(nums4) << std::endl;
-	std::cout << validMountainArray(nums5) << std::endl;
+	std::vector<int> numsReplaced{replaceElements(nums)};
 	std::cout << std::endl;
 	system("pause");
 	return 0;
