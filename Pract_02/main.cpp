1c3cbd0e54eda829460d908da6dab34921b137ff
TheGittoman <anonyme.and.free@protonmail.com>
2020-12-27 14:12:00 +0200

    New Tree Branch

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -11,27 +11,12 @@
 #include <vector>
 #include <climits>
 #include <chrono>
+#include <memory>
+#include <utility>
 
 int main()
 {
 	Timer time;
-	using namespace lc;
-	MyLinkedList *obj = new MyLinkedList();
-	obj->addAtHead(2);
-	obj->deleteAtIndex(1);
-	obj->print();
-	obj->addAtHead(2);
-	obj->addAtHead(7);
-	obj->addAtHead(3);
-	obj->addAtHead(2);
-	obj->addAtHead(5);
-	obj->print();
-	obj->addAtTail(5);
-	obj->print();
-	std::cout << obj->get(5) << std::endl;
-	obj->deleteAtIndex(6);
-	obj->deleteAtIndex(4);
-	obj->printReverse();
 	std::cout << "Time Taken To Run This Program: " << time.elapsed() << std::endl;
 	system("pause");
 	return 0;
