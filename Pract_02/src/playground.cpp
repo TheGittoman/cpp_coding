d4d975d93487d850adb08ee61e397490211328b0
TheGittoman <anonyme.and.free@protonmail.com>
2021-01-04 04:55:38 +0200

    headers and cpp added

---
+++ Pract_02/src/playground.cpp
@@ -0,0 +1,7 @@
+#include "playground.hpp"
+
+#include <iostream>
+
+namespace pg
+{
+}
---
+++ Pract_02/src/playground.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#ifndef PLAYGROUND_HPP
+#define PLAYGROUND_HPP
+
+namespace pg
+{
+}
+
+#endif
