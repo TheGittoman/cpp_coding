45400325e35282ede5f35644b965d498db77db44
TheGittoman <anonyme.and.free@protonmail.com>
2021-01-08 20:49:57 +0200

    something else

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -21,21 +21,6 @@
 int main() {
   Timer time;
   // Start timer
-  sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
-
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) window.close();
-    }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-
   // End timer
   fmt::print("Program has taken: {} seconds to run\n", time.elapsed());
   system("pause");
