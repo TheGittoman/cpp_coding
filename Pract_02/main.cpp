a664dcca7f314370c4386b6e939921609292c53c
TheGittoman <anonyme.and.free@protonmail.com>
2021-01-10 19:55:24 +0200

    Something

--- Pract_02/main.cpp
+++ Pract_02/main.cpp
@@ -1,27 +1,28 @@
 #include "src/fuf.hpp"
-// every little function that is used all the time currently only printArray functio
+// Useful functions
 #include "src/timer.hpp"
 // timer reset elapsed(which returs double value)
 
-#include <iostream>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+
+#include <algorithm>
+#include <chrono>
+#include <climits>
 #include <fstream>
+#include <iostream>
+#include <memory>
 #include <sstream>
 #include <string>
-#include <algorithm>
-#include <vector>
-#include <climits>
-#include <chrono>
-#include <memory>
 #include <utility>
-#include <fmt/core.h>
+#include <vector>
 
-int main()
-{
-	Timer time;
-	// Start timer
+int main() {
+  Timer time;
+  // Start timer
 
-	// End timer
-	fmt::print("Program took: {} seconds to run\n", time.elapsed());
-	system("pause");
-	return 0;
+  // End timer
+  fmt::print("Program took: {} seconds to run\n", time.elapsed());
+  system("pause");
+  return 0;
 }
