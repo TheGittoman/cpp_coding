5f081a78bf81af71624a31c525fc6b5888bafc91
Gittoman <anonyme.and.free@protonmail.com>
2022-01-24 09:44:34 +0200

    Defaulted the Default

--- Pract_01/src/header.cpp
+++ Pract_01/src/header.cpp
@@ -2,19 +2,3 @@
 #include <iostream>
 #include <vector>
 
-void test_Func()
-{
-    std::cout << "Hello World!" << std::endl;
-}
-
-std::string Account::getUser() const{
-    return m_user;
-}
-int Account::getID() const{
-    return m_ID;
-}
-
-void list_accounts(std::vector<Account> &accounts){
-    for(auto &element : accounts)
-        std::cout << element;
-}
--- Pract_01/src/header.hpp
+++ Pract_01/src/header.hpp
@@ -3,31 +3,6 @@
 #include <iostream>
 #include <vector>
 
-void test_Func();
 
-static int ID{};
-
-class Account
-{
-private:
-    std::string m_user{};
-    int m_ID{};
-    std::string m_pass{};
-public:
-    Account(std::string user="default_user", std::string pass="default_pass") : m_user{user}, m_pass{pass} {
-        std::cout << "User: " << user << " CREATED" << std::endl;
-        ++ID;
-        m_ID = ID;
-    }
-    std::string getUser() const;
-    int getID() const;
-
-    friend std::ostream &operator<<(std::ostream &out, Account &account){
-        out << "User: " << account.getUser() << "\nID: " << account.getID() << std::endl;
-        return out;
-    }
-};
-
-void list_accounts(std::vector<Account> &accounts);
 
 #endif
