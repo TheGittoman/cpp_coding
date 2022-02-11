fb6556f7a56762d2a6c02e551dfdd83e7114f475
Gittoman <anonyme.and.free@protonmail.com>
2022-02-11 20:04:50 +0200

    src and main added

---
+++ Project/main.cpp
@@ -0,0 +1,11 @@
+#include "src/header.hpp"
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!\n";
+  defaultFunction();
+
+  return 0;
+}
---
+++ Project/src/header.cpp
@@ -0,0 +1,8 @@
+#include "header.hpp"
+
+#include <iostream>
+
+void defaultFunction()
+{
+  std::cout << "Hello from header!\n";
+}
