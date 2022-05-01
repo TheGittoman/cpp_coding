ee5e0f83b5843591f2d6c340d6267cf4e1edc743
Gittoman <anonyme.and.free@protonmail.com>
2022-05-01 16:05:49 +0300

    projection matrix and textures added

--- Project/main.cpp
+++ Project/main.cpp
@@ -14,6 +14,13 @@
 #include "src/Shader.h"
 #include "src/Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+// int main(int argc, char **argv)
+// {
+// }
+
 int main(void)
 {
   GLFWwindow *window;
@@ -27,7 +34,7 @@ int main(void)
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Rectangle Program", NULL, NULL);
+  window = glfwCreateWindow(960, 540, "Rectangle Program", NULL, NULL);
   if (!window)
   {
     glfwTerminate();
@@ -48,23 +55,10 @@ int main(void)
 
   { // setting up scope for terminating OpenGL stuff
     float positions[]{
-        -0.5f,
-        -0.5f,
-        0.0f,
-        0.0f,
-        0.5f,
-        -0.5f,
-        1.0f,
-        0.0f,
-        0.5f,
-        0.5f,
-        1.0f,
-        1.0f,
-        -0.5f,
-        0.5f,
-        0.0f,
-        1.0f,
-    };
+        100.0f, 100.0f, 0.0f, 0.0f,
+        200.0f, 100.0f, 1.0f, 0.0f,
+        200.0f, 200.0f, 1.0f, 1.0f,
+        100.0f, 200.0f, 0.0f, 1.0f};
 
     unsigned int indices[]{
         0, 1, 2,
@@ -73,9 +67,9 @@ int main(void)
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-    unsigned int vao;
-    GLCall(glGenVertexArrays(1, &vao));
-    GLCall(glBindVertexArray(vao));
+    // unsigned int vao;
+    // GLCall(glGenVertexArrays(1, &vao));
+    // GLCall(glBindVertexArray(vao));
 
     VertexArray va;
     VertexBuffer vb(positions, 4 * 4 * sizeof(float));
@@ -87,9 +81,19 @@ int main(void)
 
     IndexBuffer ib(indices, 6);
 
+    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+    glm::mat4 mvp = proj * view * model;
+
+    // glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f); // example vector
+    // glm::vec4 result = proj * vp; // converting vector to 0-1 space
+
     Shader shader("res/shaders/basic.glsl");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+    shader.SetUniformMat4f("u_MVP", mvp);
 
     Texture texture("res/textures/logo.png");
     texture.Bind();
