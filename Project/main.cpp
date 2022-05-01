b27171d72da8ead8f177f27bec86486794f68eed
Gittoman <anonyme.and.free@protonmail.com>
2022-05-01 18:43:12 +0300

    tests done

--- Project/main.cpp
+++ Project/main.cpp
@@ -21,6 +21,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "src/tests/TestClearColor.h"
+
 // int main(int argc, char **argv)
 // {
 // }
@@ -58,51 +60,8 @@ int main(void)
   std::cout << glGetString(GL_VERSION) << std::endl;
 
   { // setting up scope for terminating OpenGL stuff
-    float positions[]{
-        -50.0f, 50.0f, 0.0f, 1.0f,
-        -50.0f, -50.0f, 0.0f, 0.0f,
-        50.0f, -50.0f, 1.0f, 0.0f,
-        50.0f, 50.0f, 1.0f, 1.0f};
-
-    unsigned int indices[]{
-        0, 1, 2,
-        2, 3, 0};
-
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-    // unsigned int vao;
-    // GLCall(glGenVertexArrays(1, &vao));
-    // GLCall(glBindVertexArray(vao));
-
-    VertexArray va;
-    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-    VertexBufferLayout layout;
-    layout.Push<float>(2);
-    layout.Push<float>(2);
-    va.AddBuffer(vb, layout);
-
-    IndexBuffer ib(indices, 6);
-
-    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-    // glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f); // example vector
-    // glm::vec4 result = proj * vp; // converting vector to 0-1 space
-
-    Shader shader("res/shaders/basic.glsl");
-    shader.Bind();
-    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-    Texture texture("res/textures/logo.png");
-    texture.Bind();
-    shader.SetUniform1i("u_Texture", 0);
-
-    va.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-    shader.Unbind();
 
     Renderer renderer;
 
@@ -112,58 +71,29 @@ int main(void)
     ImGuiIO &io = ImGui::GetIO();
     (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;   // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport / Platform Windows
-    // io.ConfigViewportsNoAutoMerge = true;
-    // io.ConfigViewportsNoTaskBarIcon = true;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    // ImGui::StyleColorsClassic();
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init((char *)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
 
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    test::TestClearColor test;
 
-    glm::vec3 translationA{200, 200, 0};
-    glm::vec3 translationB{400, 200, 0};
-
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-      /* Render here */
       renderer.Clear();
+
+      test.OnUpdate(0.0f);
+      test.OnRender();
 
       ImGui_ImplOpenGL3_NewFrame();
       ImGui_ImplGlfw_NewFrame();
       ImGui::NewFrame();
 
-      {
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-        glm::mat4 mvp = proj * view * model;
-        shader.Bind();
-        shader.SetUniformMat4f("u_MVP", mvp);
-        renderer.Draw(va, ib, shader);
-      }
-
-      {
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-        glm::mat4 mvp = proj * view * model;
-        shader.Bind();
-        shader.SetUniformMat4f("u_MVP", mvp);
-        renderer.Draw(va, ib, shader);
-      }
-
-      {
-        static float f = 0.0f;
-        ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
-        ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-      }
+      test.OnImGuiRender();
 
       ImGui::Render();
       ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -176,10 +106,7 @@ int main(void)
         glfwMakeContextCurrent(backup_current_context);
       }
 
-      /* Swap front and back buffers */
       GLCall(glfwSwapBuffers(window););
-
-      /* Poll for and process events */
       GLCall(glfwPollEvents();)
     }
   }
